110e0075b377ae90891bb3d6ad06725df50c9c80
HosenSudler <laser.boy.marcel@gmail.com>
2025-08-15 16:13:23 +0200

    v1 6

---
+++ skript.js
@@ -0,0 +1,11 @@
+const audio = new Audio("sounds/sound.wav");
+audio.load(); // Laden vorbereiten
+
+document.body.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play().then(() => {
+            audio.pause();
+            audio.currentTime = 0;
+        }).catch(() => {});
+    }
+}, { once: true }); // nur einmal
