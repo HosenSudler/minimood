a2c7e89bf605a06b65748dba9d5aab381f740a0c
HosenSudler <laser.boy.marcel@gmail.com>
2025-08-25 15:18:00 +0200

    v1 11

--- skript.js
+++ skript.js
@@ -1,16 +1,3 @@
-const audio = new Audio("sounds/sound.wav");
-audio.load(); // Laden vorbereiten
-
-document.body.addEventListener('click', () => {
-    if (audio.paused) {
-        audio.play().then(() => {
-            audio.pause();
-            audio.currentTime = 0;
-        }).catch(() => {});
-    }
-}, { once: true }); // nur einmal
-
-
 
 document.querySelectorAll('.day-btn').forEach(btn => {
   btn.addEventListener('click', () => {
